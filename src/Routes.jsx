8ae080edd8eabb4417bfa50140e6d76d00859ce1
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2024-04-27 10:37:27 +0530

    FIx:the error

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-import VijayDashboardpage from "pages/VijayDashboardpage";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import VijayDashboardpage from "./pages/VijayDashboardpage";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
